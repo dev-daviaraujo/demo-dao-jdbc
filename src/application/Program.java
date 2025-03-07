c2faff49488ccbec1d7d5d6ec1092eda7f099384
dev-daviaraujo <dev.daviaraujo@gmail.com>
2025-03-07 01:33:01 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -22,6 +23,14 @@ public class Program {
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
 		
+		System.out.println(" === TEST 3: seller findAll ===");
+		list = sellerDao.findAll();
+		list.forEach(System.out::println);
+		
+		System.out.println(" === TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New ID = " + newSeller.getId());
 	}
 	
 }
